28885abf39bacef97ac5ead46d13342ca3a1ced3
Hariton Terentyev <justsaytony@yandex.ru>
2021-08-29 08:12:15 +0300

    fix: исправление ошибок

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,37 @@
 let popup = document.querySelector('.popup');
-let popupButton = document.querySelector('.profile__info-edit-button');
-let closeButton = document.querySelector('.popup__close');
+let openPopupButton = document.querySelector('.profile__info-edit-button');
+let closePopupButton = document.querySelector('.popup__close');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__input-name');
-let jobInput = document.querySelector('.popup__input-job');
+let nameInput = document.querySelector('#nameInput');
+let jobInput = document.querySelector('#jobInput');
 let profileName = document.querySelector('.profile__info-title')
 let profileJob = document.querySelector('.profile__info-subtitle')
 
-function formSubmitHandler (evt) {
-  evt.preventDefault();
-  profileName.textContent = nameInput.value;
-  profileJob.textContent = jobInput.value;
-  togglePopup()
-}
-
-function togglePopup() {
-  popup.classList.toggle('popup_opened');
+// функция открытия popup
+function openPopup() {
+  popup.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
-popupButton.addEventListener('click', togglePopup);
-closeButton.addEventListener('click', togglePopup);
+// функция закрытия popup
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
+
+// Обработчик "отправки" формы
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  closePopup()
+}
+
+// Открытие popup
+openPopupButton.addEventListener('click', openPopup);
+
+// Закрытие popup
+closePopupButton.addEventListener('click', closePopup);
+
+// Отправка формы
 formElement.addEventListener('submit', formSubmitHandler);
